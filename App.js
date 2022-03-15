a424de52aae99b320016524687fae4a3b078254c
inzayn <hungrymindinside99@gmail.com>
2022-03-15 17:16:45 +0545

    error-fix

--- App.js
+++ App.js
@@ -17,10 +17,4 @@ export default function App() {
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
