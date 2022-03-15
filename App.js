2b51ba8c4b357b7aa954bc5b1b7fd5ea2868ac05
inzayn <hungrymindinside99@gmail.com>
2022-03-15 16:45:49 +0545

    comple-guide

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View>
+      <TextInput />
+      <Button title='ADD' />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
