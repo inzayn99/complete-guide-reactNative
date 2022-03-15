548a4ab31e2217c7d533a68e71a5a5bd535e315f
inzayn <hungrymindinside99@gmail.com>
2022-03-15 17:13:31 +0545

    InputBox with percentage

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-      <View>
-      <TextInput placeholder="Course Goal" style={{borderBottomColor: 'black', borderWidth: 1, padding: 10}}/>
-      <Button title='ADD' />
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: 'row', justifyContent: "space-between", alignItems: 'center' }}>
+        <TextInput placeholder="Course Goals" style={{ width: '80%', borderBottomColor: 'black', borderWidth: 1, padding: 10 }} />
+        <Button title='ADD' />
       </View>
       <View>
 
